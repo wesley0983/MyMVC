62c17d96efdeb631772aa63a0f3dd4339b7e86c2
wesley <w093578578888@gmail.com>
2018-12-13 22:29:37 +0800

    123

--- src/home_class/Auto_Product.java
+++ src/home_class/Auto_Product.java
@@ -1,9 +1,13 @@
 package home_class;
 
+import java.util.Scanner;
+
 public class Auto_Product {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = "Insert into PRODUCT (PRO_NO,PRO_CLASSID,PRO_NAME,PRO_PIC,PRO_PIC_EXT,PRO_FORMAT,PRO_BONUS,PRO_STOCK,PRO_SAFESTOCK,PRO_DETAILS,PRO_SHELVE,PRO_ALL_ASSESS,PRO_ALL_ASSESSMAN) values ('P'||LPAD(to_char(PRODUCT_SEQ.NEXTVAL), 3, '0'),?,?,?,?,?,?,?,?,?,?,?,?);";
 
 	}
 
