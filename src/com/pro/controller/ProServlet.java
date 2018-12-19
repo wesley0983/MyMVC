e93c96a44b6cfad15d2fa4657562b723fa94cdbd
wesley <w093578578888@gmail.com>
2018-12-19 19:20:46 +0800

    已完成錯誤測試-尚未更改名稱

--- src/com/pro/controller/ProServlet.java
+++ src/com/pro/controller/ProServlet.java
@@ -43,7 +43,7 @@ if ("getOne_For_Display".equals(action)) { //來自select_page.jsp的請求
 				String str = req.getParameter("pro_no");
 
 				if (str == null || (str.trim()).length() == 0) {
-					errorMsgs.add("請輸入員工編號");
+					errorMsgs.add("請輸入商品編號");
 				}
 				// Send the use back to the form, if there were errors
 				if (!errorMsgs.isEmpty()) {
@@ -133,42 +133,65 @@ if ("update".equals(action)) { // 來自update_emp_input.jsp的請求
 			// Store this set in the request scope, in case we need to
 			// send the ErrorPage view.
 			req.setAttribute("errorMsgs", errorMsgs);
-			System.out.println("有近來");
+		
 		    
 			try {
 				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String pro_no = req.getParameter("pro_no");
+				String pro_classid = req.getParameter("pro_classid");				
+				String pro_name = req.getParameter("ename").trim();
+				if (pro_name == null || pro_name.trim().length() == 0) {
+					errorMsgs.add("名稱請勿空白");
+				}	
 				//解析網頁送來的圖片
-				Part part = req.getPart("pro_pic");			
+				Part part = req.getPart("pro_pic");				
 				InputStream in = part.getInputStream();	
 				byte[] pro_pic = ProServlet.Photo(in);
-				
-				
-				String pro_no = req.getParameter("pro_no");
-				String pro_classid = req.getParameter("pro_classid");
-				String pro_name = req.getParameter("ename");
-				String pro_pic_ext = req.getParameter("pic_ext");
+				if (pro_pic == null || pro_pic.length == 0) {
+					errorMsgs.add("請上傳圖片");
+				}
+			
+				String pro_pic_ext = req.getParameter("pic_ext");//不需要
+					
 				String pro_format = req.getParameter("format");
+				if (pro_format == null || pro_format.trim().length() == 0) {
+					errorMsgs.add("商品規格請勿空白");
+				}
+				
 				Integer pro_bonus = new Integer(req.getParameter("bonus").trim());
+				if (pro_bonus == null || pro_bonus == 0) {
+					errorMsgs.add("商品單價請勿空白");
+				}
+				
 				Integer pro_stock = new Integer(req.getParameter("stock").trim());
+				if (pro_stock == null || pro_stock == 0) {
+					errorMsgs.add("商品庫存請勿空白");
+				}
+				
 				Integer pro_safestock = new Integer(req.getParameter("stock").trim());
+				if (pro_safestock == null || pro_safestock == 0) {
+					errorMsgs.add("商品安全庫存請勿空白");
+				}
+				
 				String pro_details = req.getParameter("details");
+				if (pro_details == null || pro_details.trim().length() == 0) {
+					errorMsgs.add("商品詳述請勿空白");
+				}
+				
 				String pro_shelve = req.getParameter("shelve");
+				if (pro_details == null || pro_details.trim().length() == 0) {
+					errorMsgs.add("商品狀態請勿空白");
+				}
+				
 				Integer pro_all_assess = new Integer(req.getParameter("assess").trim());
+				if (pro_all_assess == null || pro_all_assess == 0) {
+					errorMsgs.add("商品總評價請勿空白");
+				}
+				
 				Integer pro_all_assessman = new Integer(req.getParameter("assessman").trim());
-				
-				
-				System.out.println("pro_no :" + pro_no);
-				System.out.println("pro_no :" + pro_classid);
-				System.out.println("pro_no :" + pro_name);
-				System.out.println("pro_no :" + pro_pic_ext);
-				System.out.println("pro_no :" + pro_format);
-				System.out.println("pro_no :" + pro_bonus);
-				System.out.println("pro_no :" + pro_stock);
-				System.out.println("pro_no :" + pro_safestock);
-				System.out.println("pro_no :" + pro_shelve);
-				System.out.println("pro_no :" + pro_all_assess);
-				System.out.println("pro_no :" + pro_all_assessman);
-				
+				if (pro_all_assess == null || pro_all_assess == 0) {
+					errorMsgs.add("商品評價總人數請勿空白");
+				}
 				
 				
 				
@@ -231,33 +254,68 @@ if ("insert".equals(action)) { //來自addEmp.jsp的請求
 				Part part = req.getPart("pro_pic");			
 				InputStream in = part.getInputStream();	
 				byte[] pro_pic = ProServlet.Photo(in);
+				if (pro_pic == null || pro_pic.length == 0) {
+					errorMsgs.add("請上傳圖片");
+				}
+				
 				//拿取網頁資料
 				String pro_classid = req.getParameter("pro_classid");
-				String ename = req.getParameter("ename");
+				if (pro_classid == null || pro_classid.trim().length() == 0) {
+					errorMsgs.add("商品類別請勿空白");
+				}
 				
+				String pro_name = req.getParameter("ename");
+				if (pro_name == null || pro_name.trim().length() == 0) {
+					errorMsgs.add("名稱請勿空白");
+				}
 				String pic_ext = req.getParameter("pic_ext");
-				String format = req.getParameter("format");
-				Integer bonus = new Integer(req.getParameter("bonus").trim());
-				Integer stock = new Integer(req.getParameter("stock").trim());
-				Integer safestock = new Integer(req.getParameter("stock").trim());
-				String details = req.getParameter("details");
-				String shelve = req.getParameter("shelve");
-				Integer assess = new Integer(req.getParameter("assess").trim());
-				Integer assessman = new Integer(req.getParameter("assessman").trim());
+				
+				String pro_format = req.getParameter("format");
+				if (pro_format == null || pro_format.trim().length() == 0) {
+					errorMsgs.add("商品規格請勿空白");
+				}
+				Integer pro_bonus = new Integer(req.getParameter("bonus").trim());
+				if (pro_bonus == null || pro_bonus == 0) {
+					errorMsgs.add("商品單價請勿空白");
+				}
+				Integer pro_stock = new Integer(req.getParameter("stock").trim());
+				if (pro_stock == null || pro_stock == 0) {
+					errorMsgs.add("商品庫存請勿空白");
+				}
+				Integer pro_safestock = new Integer(req.getParameter("stock").trim());
+				if (pro_safestock == null || pro_safestock == 0) {
+					errorMsgs.add("商品安全庫存請勿空白");
+				}
+				String pro_details = req.getParameter("details");
+				if (pro_details == null || pro_details.trim().length() == 0) {
+					errorMsgs.add("商品詳述請勿空白");
+				}
+				String pro_shelve = req.getParameter("shelve");
+				if (pro_shelve == null || pro_shelve.trim().length() == 0) {
+					errorMsgs.add("商品狀態請勿空白");
+				}
+				Integer pro_all_assess = new Integer(req.getParameter("assess").trim());
+				if (pro_all_assess == null || pro_all_assess == 0) {
+					errorMsgs.add("商品總評價請勿空白");
+				}
+				Integer pro_all_assessman = new Integer(req.getParameter("assessman").trim());
+				if (pro_all_assessman == null || pro_all_assessman == 0) {
+					errorMsgs.add("商品總評價請勿空白");
+				}
 
 				ProductVO proVO = new ProductVO();
 				proVO.setPro_classid(pro_classid);
-		        proVO.setPro_name(ename);
+		        proVO.setPro_name(pro_name);
 		        proVO.setPro_pic(pro_pic);//圖片給空
 		        proVO.setPro_pic_ext(pic_ext);
-		        proVO.setPro_format(format);
-		        proVO.setPro_bonus(bonus);
-		        proVO.setPro_stock(stock);
-		        proVO.setPro_safestock(safestock);
-		    	proVO.setPro_details(details);
-		    	proVO.setPro_shelve(shelve);
-		    	proVO.setPro_all_assess(assess);
-		    	proVO.setPro_all_assessman(assessman);
+		        proVO.setPro_format(pro_format);
+		        proVO.setPro_bonus(pro_bonus);
+		        proVO.setPro_stock(pro_stock);
+		        proVO.setPro_safestock(pro_safestock);
+		    	proVO.setPro_details(pro_details);
+		    	proVO.setPro_shelve(pro_shelve);
+		    	proVO.setPro_all_assess(pro_all_assess);
+		    	proVO.setPro_all_assessman(pro_all_assessman);
 		    	
 
 
@@ -272,7 +330,8 @@ if ("insert".equals(action)) { //來自addEmp.jsp的請求
 				
 				/***************************2.開始新增資料***************************************/
 				ProductService proSvc = new ProductService();
-				proVO = proSvc.addPro(pro_classid,ename,pro_pic,pic_ext,format,bonus,stock,safestock,details,shelve,assess,assessman);
+				proVO = proSvc.addPro(pro_classid,pro_name,pro_pic,pic_ext,pro_format,
+						pro_bonus,pro_stock,pro_safestock,pro_details,pro_shelve,pro_all_assess,pro_all_assessman);
 				
 				
 				/***************************3.新增完成,準備轉交(Send the Success view)***********/
@@ -313,7 +372,7 @@ if ("delete".equals(action)) { // �Ӧ�listAllEmp.jsp
 				
 				/***************************其他可能的錯誤處理**********************************/
 			} catch (Exception e) {
-				errorMsgs.add("刪除資料失敗:"+e.getMessage());
+				errorMsgs.add("刪除資料失敗:訂單或商品促銷專案未撤銷。"+e.getMessage());
 				RequestDispatcher failureView = req
 						.getRequestDispatcher("/pro/listAllEmp.jsp");
 				failureView.forward(req, res);
